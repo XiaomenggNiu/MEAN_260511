535758d68dd3d84eaa2c44d25657789b7abe4789
XiaomenggNiu <nxmeng0000@gmail.com>
2026-05-27 16:17:32 -0400

    Lifecycle hooks intro

--- inClass/src/app/app.ts
+++ inClass/src/app/app.ts
@@ -2,14 +2,20 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './first-component/first-component';
 import { DirectivesIntroComponent } from './directives-intro-component/directives-intro-component';
+import { LifecycleIntro } from './lifecycle-intro/lifecycle-intro';
 // import {FirstComponent} from './first-component/first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent, DirectivesIntroComponent],
+  imports: [RouterOutlet, FirstComponent, DirectivesIntroComponent, LifecycleIntro],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('inClass');
+  condition = true;
+
+  toggle(){
+    this.condition = !this.condition;
+  }
 }
