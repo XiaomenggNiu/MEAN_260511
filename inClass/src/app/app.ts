2c2d394181e5d8e0746f2ad88b175e1138f34acd
XiaomenggNiu <nxmeng0000@gmail.com>
2026-05-27 15:21:14 -0400

    directives intro

--- inClass/src/app/app.ts
+++ inClass/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './first-component/first-component';
+import { DirectivesIntroComponent } from './directives-intro-component/directives-intro-component';
 // import {FirstComponent} from './first-component/first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent],
+  imports: [RouterOutlet, FirstComponent, DirectivesIntroComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- inClass/src/app/first-component/first-component.ts
+++ inClass/src/app/first-component/first-component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first-component.html',
   styleUrl: './first-component.scss',
 })
 export class FirstComponent {
   data = 'Miranda';
+  disable = false;
 
+  userInput = "Initial Input";
 
-  onClick(){
-    this.data = "Tom";
+  onClick() {
+    this.data = 'Tom';
+    this.disable = true;
   }
-
-
 }
