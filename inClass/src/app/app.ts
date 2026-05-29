b064417a6ea081298dbcbd561e71bc807bad211d
XiaomenggNiu <nxmeng0000@gmail.com>
2026-05-29 15:38:27 -0400

    HttpClient + Observable

--- inClass/src/app/app.ts
+++ inClass/src/app/app.ts
@@ -5,18 +5,26 @@ import { DirectivesIntroComponent } from './directives-intro-component/directive
 import { LifecycleIntro } from './lifecycle-intro/lifecycle-intro';
 // import {FirstComponent} from './first-component/first-component';
 import { Day3Component } from './day3-component/day3-component';
+import { Day4Component } from './day4-component/day4-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Day3Component, FirstComponent, DirectivesIntroComponent, LifecycleIntro],
+  imports: [
+    RouterOutlet,
+    Day3Component,
+    FirstComponent,
+    DirectivesIntroComponent,
+    LifecycleIntro,
+    Day4Component,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('inClass');
   condition = true;
 
-  toggle(){
+  toggle() {
     this.condition = !this.condition;
   }
 }
